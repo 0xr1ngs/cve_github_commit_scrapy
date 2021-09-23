f2af918ec8168788c9cef34aee3af44004e7acd6
r1ngs <r1ngs@outlook.com>
2021-09-23 09:49:22 +0800

    bug fix

--- cve_scrapy/cve_scrapy/pipelines.py
+++ cve_scrapy/cve_scrapy/pipelines.py
@@ -12,29 +12,31 @@ import requests
 
 class CveScrapyPipeline:
     def process_item(self, item, spider):
-        home_path = os.environ['HOME']
-        # home_path = 'var/tmp'
+        # home_path = os.environ['HOME']
+        home_path = 'D://tmp'
         file_name = item['file'].split('/')[-1]
         old_file_path = home_path + '/' + item['keyword'] + '/' + item['year'] + '/' + item[
             'cve_name'] + '/' + file_name + '.old'
+        file_dir = home_path + '/' + item['keyword'] + '/' + item['year'] + '/' + item[
+            'cve_name'] + '/'
         new_file_path = home_path + '/' + item['keyword'] + '/' + item['year'] + '/' + item[
             'cve_name'] + '/' + file_name + '.new'
         README_path = home_path + '/' + item['keyword'] + '/' + item['year'] + '/' + item['cve_name'] + '/README.txt'
 
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+
         if not os.path.exists(old_file_path):
             old_file_code = requests.get(item['old_file_url']).content
-            os.mknod(old_file_path)
             with open(old_file_path, "w") as f:
-                f.write(old_file_code)
+                f.write(old_file_code.decode('utf-8'))
 
         if not os.path.exists(new_file_path):
             new_file_code = requests.get(item['new_file_url']).content
-            os.mknod(new_file_path)
             with open(new_file_path, "w") as f:
-                f.write(new_file_code)
+                f.write(new_file_code.decode('utf-8'))
 
         if not os.path.exists(README_path):
-            README_content = item
-            os.mknod(README_path)
-            with open(README_path, "w") as f:
+            README_content = 'file:' + item['file']
+            with open(README_path, "wa") as f:
                 f.write(README_content)
